fbdff6b8a2c9ff8e477504893822dfa2a4ddd406
jasoncheng <goodice0728@gmail.com>
2024-01-14 20:46:11 +0800

    加入新增刪除介面

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,5 +1,29 @@
 <template>
-  <el-tree :data="menus" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+  <el-tree :data="menus" :props="defaultProps"
+           :expand-on-click-node="false" show-checkbox node-key="catId"
+  >
+
+     <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button
+            v-if="node.level <=2"
+            type="text"
+            size="mini"
+            @click="() => append(data)">
+            Append
+          </el-button>
+          <el-button
+            v-if="node.childNodes.length===0"
+            type="text"
+            size="mini"
+            @click="() => remove(node, data)">
+            Delete
+          </el-button>
+        </span>
+      </span>
+
+  </el-tree>
 </template>
 
 <script>
@@ -19,6 +43,12 @@ export default {
   methods: {
     handleNodeClick (data) {
       console.log(data)
+    },
+    append (data) {
+      console.log('append', data)
+    },
+    remove (node, data) {
+      console.log('remove', node, data)
     },
     getMenus () {
       this.dataListLoading = true
